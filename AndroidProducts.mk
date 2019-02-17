2bdad5444dedd2c9974e5bd05eb84eb609be8f43
RinKirito <abdul.4pda05@yandex.com>
2019-02-17 06:58:47 +0000

    AOSCP bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,2 @@
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/aosp_marlin.mk \
-	$(LOCAL_DIR)/aosp_marlin_svelte.mk \
-	$(LOCAL_DIR)/aosp_sailfish.mk \
-	$(LOCAL_DIR)/bootleg_marlin.mk \
-	$(LOCAL_DIR)/bootleg_sailfish.mk
+	$(LOCAL_DIR)/aoscp_.mk
--- aoscp.mk
+++ aoscp.mk
@@ -3,7 +3,7 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
 # Inherit Bootleggers product configuration
-$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
+$(call inherit-product, vendor/aoscp/configs/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/marlin/aosp_sailfish.mk)
@@ -11,7 +11,7 @@ $(call inherit-product, device/google/marlin/aosp_sailfish.mk)
 include device/google/marlin/device-bootleg.mk
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := bootleg_sailfish
+PRODUCT_NAME := aoscp_sailfish
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel
 TARGET_MANUFACTURER := Google
@@ -22,6 +22,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="sailfish-user 9 PQ2A.190205.003 5180536 release-keys"
 
 BUILD_FINGERPRINT := google/sailfish/sailfish:9/PQ2A.190205.003/5180536:user/release-keys
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.bootleggers.maintainer=shagbag913
